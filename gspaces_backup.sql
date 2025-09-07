06e96da52ab8439e215aa1656689cb6fa25a78f7
sreekanthops <sri.chityala501@gmail.com>
2025-09-07 17:55:27 +0530

    Update gspaces_backup.sql

--- gspaces_backup.sql
+++ gspaces_backup.sql
@@ -21,6 +21,42 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: cart; Type: TABLE; Schema: public; Owner: sri
+--
+
+CREATE TABLE public.cart (
+    id integer NOT NULL,
+    user_id integer,
+    product_id integer,
+    quantity integer DEFAULT 1
+);
+
+
+ALTER TABLE public.cart OWNER TO sri;
+
+--
+-- Name: cart_id_seq; Type: SEQUENCE; Schema: public; Owner: sri
+--
+
+CREATE SEQUENCE public.cart_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.cart_id_seq OWNER TO sri;
+
+--
+-- Name: cart_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: sri
+--
+
+ALTER SEQUENCE public.cart_id_seq OWNED BY public.cart.id;
+
+
+--
 -- Name: order_items; Type: TABLE; Schema: public; Owner: sri
 --
 
@@ -97,6 +133,43 @@ ALTER SEQUENCE public.orders_id_seq OWNER TO sri;
 --
 
 ALTER SEQUENCE public.orders_id_seq OWNED BY public.orders.id;
+
+
+--
+-- Name: product_sub_images; Type: TABLE; Schema: public; Owner: sri
+--
+
+CREATE TABLE public.product_sub_images (
+    id integer NOT NULL,
+    product_id integer NOT NULL,
+    image_url text NOT NULL,
+    description text,
+    created_at timestamp without time zone DEFAULT CURRENT_TIMESTAMP
+);
+
+
+ALTER TABLE public.product_sub_images OWNER TO sri;
+
+--
+-- Name: product_sub_images_id_seq; Type: SEQUENCE; Schema: public; Owner: sri
+--
+
+CREATE SEQUENCE public.product_sub_images_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.product_sub_images_id_seq OWNER TO sri;
+
+--
+-- Name: product_sub_images_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: sri
+--
+
+ALTER SEQUENCE public.product_sub_images_id_seq OWNED BY public.product_sub_images.id;
 
 
 --
@@ -218,6 +291,13 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: cart id; Type: DEFAULT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.cart ALTER COLUMN id SET DEFAULT nextval('public.cart_id_seq'::regclass);
+
+
+--
 -- Name: order_items id; Type: DEFAULT; Schema: public; Owner: sri
 --
 
@@ -229,6 +309,13 @@ ALTER TABLE ONLY public.order_items ALTER COLUMN id SET DEFAULT nextval('public.
 --
 
 ALTER TABLE ONLY public.orders ALTER COLUMN id SET DEFAULT nextval('public.orders_id_seq'::regclass);
+
+
+--
+-- Name: product_sub_images id; Type: DEFAULT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.product_sub_images ALTER COLUMN id SET DEFAULT nextval('public.product_sub_images_id_seq'::regclass);
 
 
 --
@@ -253,11 +340,29 @@ ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_
 
 
 --
+-- Data for Name: cart; Type: TABLE DATA; Schema: public; Owner: sri
+--
+
+COPY public.cart (id, user_id, product_id, quantity) FROM stdin;
+3	14	7	1
+9	12	7	1
+10	15	7	1
+11	16	7	1
+\.
+
+
+--
 -- Data for Name: order_items; Type: TABLE DATA; Schema: public; Owner: sri
 --
 
 COPY public.order_items (id, order_id, product_id, quantity, price_at_purchase, product_name, image_url) FROM stdin;
 1	4	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
+2	5	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
+3	6	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
+4	7	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
+5	8	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
+6	9	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
+7	10	7	1	1.00	Green Wall Desk	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png
 \.
 
 
@@ -267,6 +372,21 @@ COPY public.order_items (id, order_id, product_id, quantity, price_at_purchase, 
 
 COPY public.orders (id, user_id, order_date, total_amount, status, user_email, razorpay_order_id, razorpay_payment_id) FROM stdin;
 4	9	2025-08-23 18:57:13.961535	1.00	Completed	sri@gmail.com	order_R8sn2pwhlyf26d	pay_R8snF3eP1pNFcA
+5	14	2025-08-23 20:16:01.177902	1.00	Completed	srichityala501@gmail.com	order_R8u83hCdEGGxiW	pay_R8u8ThGoiLNCSQ
+6	12	2025-08-27 15:53:56.139289	1.00	Completed	sri.chityala504@gmail.com	order_RAPnsahBFM5obN	pay_RAPo5nJs2jYAUa
+7	12	2025-08-27 15:58:14.752533	1.00	Completed	sri.chityala504@gmail.com	order_RAPsPEz2y2RmSf	pay_RAPsfzu7rI5c0a
+8	12	2025-08-27 16:03:32.792834	1.00	Completed	sri.chityala504@gmail.com	order_RAPy5aWg435Vip	pay_RAPyG6CrMeU92b
+9	12	2025-08-27 16:09:56.675074	1.00	Completed	sri.chityala504@gmail.com	order_RAQ4nHTNhvkU6F	pay_RAQ50eT4ga4Lm2
+10	12	2025-08-27 17:50:39.700312	1.00	Completed	sri.chityala504@gmail.com	order_RARmqXBIle0Oj2	pay_RARnNlvgPS8l3f
+\.
+
+
+--
+-- Data for Name: product_sub_images; Type: TABLE DATA; Schema: public; Owner: sri
+--
+
+COPY public.product_sub_images (id, product_id, image_url, description, created_at) FROM stdin;
+1	10	img/Products/desk1.jpg	desk2	2025-09-07 12:19:08.040216
 \.
 
 
@@ -277,12 +397,12 @@ COPY public.orders (id, user_id, order_date, total_amount, status, user_email, r
 COPY public.products (id, name, description, category, price, rating, image_url, created_by) FROM stdin;
 8	GlowSpace	A sleek minimalist desk with warm ambient lighting – where work meets inspiration	executive	55000.0	4.0	img/Products/Screenshot_2025-08-16_at_10.53.22_PM.png	sri@gmail.com
 9	Scandi Minimal	Balanced simplicity with warm wood, neutral tones, and clutter-free design for a calming work vibe.	minimalist	35000.0	5.0	img/Products/f996ebea3a130d8dd1bb5b2f1f938455.jpg	sri@gmail.com
-10	Neo Ergonomic Desk	Clean, modern workspace with an ergonomic chair, sleek desk, and fresh greenery for comfort and focus.	Ergonomic	58000.0	5.0	img/Products/DSC06916.jpg	sri@gmail.com
 11	Dual Harmony	A stylish side-by-side desk setup designed for couples who work, create, or study together. This shared workspace balances productivity with harmony.	couple	99000.0	5.0	img/Products/dualdesks_1.jpg	sri@gmail.com
-7	Green Wall Desk	A modern desk setup blending greenery with minimal design — perfect for focus and calm	ergonomic	1	4.0	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png	sri@gmail.com
 12	Neon Dreamscape	The vibrant blue illumination and geometric wall panels create a dynamic and stylish space, ideal for both high-energy gaming and creative endeavors.	gaming	75000.0	4.0	img/Products/Screenshot_2025-08-19_at_4.35.56_AM.png	srichityala501@gmail.com
 13	Serene Balcony Nook	Designed for tranquility and concentration, this balcony workspace combines natural light and simple decor to offer a peaceful retreat for daily tasks	balcony	38000.0	5.0	img/Products/balcony.jpg	srichityala501@gmail.com
 14	Minimalist Balcony	Featuring clean lines, neutral tones, and essential elements, this design maximizes a compact balcony to create an efficient and aesthetically pleasing remote work	balcony	32000.0	5.0	img/Products/972b8295b1e88af3a3389a606f900d78.jpg	srichityala501@gmail.com
+10	Neo Ergonomic Desk	Clean, modern workspace with an ergonomic chair, sleek desk, and fresh greenery for comfort and focus.	executive	58000.0	5.0	img/Products/grey_magic.JPG	sri@gmail.com
+7	Green Wall Desk	A modern desk setup blending greenery with minimal design — perfect for focus and calergonomic	65000	4.0	img/Products/Screenshot_2025-08-16_at_10.48.06_PM.png	sri@gmail.com
 \.
 
 
@@ -292,6 +412,7 @@ COPY public.products (id, name, description, category, price, rating, image_url,
 
 COPY public.reviews (id, product_id, user_id, username, rating, comment, created_at) FROM stdin;
 1	7	\N	sri@gmail.com	5	Very comfortable 	2025-08-16 17:41:13.894303
+2	7	14	chityala srikanth	4	good	2025-08-24 09:42:23.63162
 \.
 
 
@@ -306,21 +427,37 @@ COPY public.users (id, name, email, password, address, phone) FROM stdin;
 12	Home	sri.chityala504@gmail.com		\N	\N
 13	Sri ch	sri.chityala500@gmail.com	hello	\N	\N
 14	chityala srikanth	srichityala501@gmail.com		\N	\N
+15	Sreekanth Devops	sreekanththetechie@gmail.com	oauth_user_no_password_6lUorcIT7qfGlC3V	\N	\N
+16	yamini chityala	chityalayamini@gmail.com	oauth_user_no_password_d69XXaVVlKg5aIG2	\N	\N
 \.
+
+
+--
+-- Name: cart_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sri
+--
+
+SELECT pg_catalog.setval('public.cart_id_seq', 12, true);
 
 
 --
 -- Name: order_items_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sri
 --
 
-SELECT pg_catalog.setval('public.order_items_id_seq', 1, true);
+SELECT pg_catalog.setval('public.order_items_id_seq', 7, true);
 
 
 --
 -- Name: orders_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sri
 --
 
-SELECT pg_catalog.setval('public.orders_id_seq', 4, true);
+SELECT pg_catalog.setval('public.orders_id_seq', 10, true);
+
+
+--
+-- Name: product_sub_images_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sri
+--
+
+SELECT pg_catalog.setval('public.product_sub_images_id_seq', 1, true);
 
 
 --
@@ -334,14 +471,30 @@ SELECT pg_catalog.setval('public.products_id_seq', 14, true);
 -- Name: reviews_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sri
 --
 
-SELECT pg_catalog.setval('public.reviews_id_seq', 1, true);
+SELECT pg_catalog.setval('public.reviews_id_seq', 2, true);
 
 
 --
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: sri
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 14, true);
+SELECT pg_catalog.setval('public.users_id_seq', 16, true);
+
+
+--
+-- Name: cart cart_pkey; Type: CONSTRAINT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.cart
+    ADD CONSTRAINT cart_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: cart cart_user_product_unique; Type: CONSTRAINT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.cart
+    ADD CONSTRAINT cart_user_product_unique UNIQUE (user_id, product_id);
 
 
 --
@@ -377,6 +530,14 @@ ALTER TABLE ONLY public.orders
 
 
 --
+-- Name: product_sub_images product_sub_images_pkey; Type: CONSTRAINT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.product_sub_images
+    ADD CONSTRAINT product_sub_images_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: sri
 --
 
@@ -409,6 +570,22 @@ ALTER TABLE ONLY public.users
 
 
 --
+-- Name: cart cart_product_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.cart
+    ADD CONSTRAINT cart_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.products(id) ON DELETE CASCADE;
+
+
+--
+-- Name: cart cart_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.cart
+    ADD CONSTRAINT cart_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
 -- Name: order_items order_items_order_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sri
 --
 
@@ -438,6 +615,14 @@ ALTER TABLE ONLY public.orders
 
 ALTER TABLE ONLY public.orders
     ADD CONSTRAINT orders_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE;
+
+
+--
+-- Name: product_sub_images product_sub_images_product_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sri
+--
+
+ALTER TABLE ONLY public.product_sub_images
+    ADD CONSTRAINT product_sub_images_product_id_fkey FOREIGN KEY (product_id) REFERENCES public.products(id) ON DELETE CASCADE;
 
 
 --

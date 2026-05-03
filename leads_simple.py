c7cb0b8a327b97de3588b5bcdd52baa30186bee7
sreekanth chityala <sreekanth.chityala@ibm.com>
2026-05-03 21:10:44 +0530

    Fix JSONB parsing in quotation view - enable media carousel for customers

--- leads_simple.py
+++ leads_simple.py
@@ -661,12 +661,17 @@ def view_quotation(share_token):
         else:
             design['custom_items'] = []
         
-        # Parse media_files
+        # Parse media_files (JSONB is already parsed by psycopg2)
         if design.get('media_files'):
-            try:
-                design['media_files'] = json.loads(design['media_files'])
-            except:
-                design['media_files'] = []
+            # If it's already a list (JSONB), use it directly
+            if isinstance(design['media_files'], list):
+                pass  # Already parsed
+            elif isinstance(design['media_files'], str):
+                # If it's a string, parse it
+                try:
+                    design['media_files'] = json.loads(design['media_files'])
+                except:
+                    design['media_files'] = []
         else:
             design['media_files'] = []
         
